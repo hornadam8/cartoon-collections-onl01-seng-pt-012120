97dcdbfb495e54b36c9d144efbe3262772a15819
Adam G Horn <hornadam8@gmail.com>
2020-01-27 20:01:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |word|
-    cheese_types.include?(#{word})
+    cheese_types.include?(word)
   end
 end
